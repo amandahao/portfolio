fd5cd827d73fc33ca757674a9d12a92dc5ee456b
Amanda Hao <amandahao@gmail.com>
2025-03-04 23:35:34 +0900

    initial text

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,28 @@ function App() {
   return (
     <>
       <SplineScene />
+      <div className="app">
+        <main>
+          <span className="caption" style={{ color: "#ffffffc9" }}>
+            Hi, I'm{" "}
+            <span
+              style={{
+                color: "#ffffff",
+              }}
+            >
+              Amanda.
+            </span>
+          </span>
+          <span
+            className="sub-caption"
+            style={{
+              color: "#ffffffe6",
+            }}
+          >
+            Science, Tech, & Intl Affairs @ Georgetown
+          </span>
+        </main>
+      </div>
     </>
   );
 }
