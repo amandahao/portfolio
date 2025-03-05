e8b75299a14e1c02ba65733ffd71b51483d1e5b9
Amanda Hao <amandahao@gmail.com>
2025-03-05 21:50:18 +0900

    initial home page

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,43 @@ function App() {
           >
             Science, Tech, & Intl Affairs @ Georgetown
           </span>
+          <span
+            className="desc"
+            style={{ color: "#ffffffcc", padding: "60px 0px 0px 0px" }}
+          >
+            Click on the computer to check out my work, or hover over other
+            components to learn more about me!
+          </span>
+          <div style={{ padding: "25px 0px 0px 0px" }}>
+            <div className="socmed">
+              <a href="https://www.linkedin.com/in/amandahao/">
+                <img
+                  src="/src/assets/linkedin.png"
+                  alt="linkedin"
+                  width="35"
+                  height="35"
+                />
+              </a>
+            </div>
+            <div className="socmed">
+              <img
+                src="/src/assets/resume.png"
+                alt="resume"
+                width="35"
+                height="35"
+              />
+            </div>
+            <div className="socmed">
+              <a href="mailto:amandahao@gmail.com">
+                <img
+                  src="/src/assets/email.png"
+                  alt="resume"
+                  width="35"
+                  height="35"
+                />
+              </a>
+            </div>
+          </div>
         </main>
       </div>
     </>
