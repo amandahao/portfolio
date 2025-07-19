01544c72046ffb653254c22578e88f23aa95942d
Amanda Hao <amandahao@gmail.com>
2025-07-19 12:01:30 -0700

    blank

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,10 @@
-import { StrictMode } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AboutPage from "./About.tsx";
-import WorkPage from "./Work.tsx";
-import ProjectDetail from "./ProjectDetail.tsx";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/work" element={<WorkPage />} />
-        <Route path="/work/:projectId" element={<ProjectDetail />} />
-      </Routes>
-    </Router>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
