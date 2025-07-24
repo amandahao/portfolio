e49df0cea05bf81f15acba0d88814288feee9e72
Amanda Hao <amandahao@gmail.com>
2025-07-23 20:25:34 -0700

    fix nvm node issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import crypto from "crypto";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
+
+// Temporary patch for bad `crypto.hash` usage
+if (typeof (crypto as any).hash !== "function") {
+  (crypto as any).hash = (
+    algo: string,
+    data: string,
+    enc: "hex" | "binary" | "base64" = "hex"
+  ) => crypto.createHash(algo).update(data).digest(enc);
+}
